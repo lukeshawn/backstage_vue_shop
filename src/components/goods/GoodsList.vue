bd15fc596c39c62cf305d86f85419cfa5c2cf8c4
lukeshawn <lukeshawn1992@gmail.com>
2020-04-07 10:44:26 +0800

    完成了列表页

--- src/components/goods/GoodsList.vue
+++ src/components/goods/GoodsList.vue
@@ -1,5 +1,124 @@
 <template>
-    <div>
-        list页面
-    </div>
+  <div>
+    <!-- 面包屑 -->
+    <el-breadcrumb separator-class="el-icon-arrow-right">
+      <el-breadcrumb-item :to="{ path: '/welcome' }">首页</el-breadcrumb-item>
+      <el-breadcrumb-item>商品管理</el-breadcrumb-item>
+      <el-breadcrumb-item>商品列表</el-breadcrumb-item>
+    </el-breadcrumb>
+    <!-- 卡片 -->
+    <el-card>
+      <!-- 搜索 -->
+      <el-row :gutter="20">
+        <el-col :span="8">
+          <el-input placeholder="请输入内容" clearable v-model="queryInfo.query" @clear="getGoodsList">
+            <el-button icon="el-icon-search" slot="append" @click="getGoodsList"></el-button>
+          </el-input>
+        </el-col>
+        <el-col :span="4">
+          <el-button type="primary" @click="goAddpage">添加用户</el-button>
+        </el-col>
+      </el-row>
+
+      <!-- 表格 -->
+      <el-table border stripe :data="goodslist">
+        <el-table-column type="index"></el-table-column>
+        <el-table-column prop="goods_name" label="商品名称"></el-table-column>
+        <el-table-column prop="goods_price" label="商品价格(元)" width="95px"></el-table-column>
+        <el-table-column prop="goods_weight" label="商品重量" width="70px"></el-table-column>
+        <el-table-column prop="add_time" label="创建时间" width="140px">
+            <template slot-scope="scope">
+                {{scope.row.add_time | dateFormat}}
+            </template>
+        </el-table-column>
+        <el-table-column label="操作" width="130px">
+          <template slot-scope="scope">
+            <el-button type="primary" icon="el-icon-edit" size="mini"></el-button>
+            <el-button
+              type="danger"
+              icon="el-icon-delete"
+              size="mini"
+              @click="removeById(scope.row.goods_id)"
+            ></el-button>
+          </template>
+        </el-table-column>
+      </el-table>
+
+      <!-- 分页 -->
+      <el-pagination
+        @size-change="handleSizeChange"
+        @current-change="handleCurrentChange"
+        :current-page="queryInfo.pagenum"
+        :page-sizes="[5, 10,15,20]"
+        :page-size="queryInfo.pagesize"
+        layout="total, sizes, prev, pager, next, jumper"
+        :total="total"
+        background
+      ></el-pagination>
+    </el-card>
+  </div>
 </template>
+
+<script>
+export default {
+  data() {
+    return {
+      queryInfo: {
+        query: '',
+        pagenum: 1,
+        pagesize: 10
+      },
+      goodslist: [],
+      total: 0
+    }
+  },
+  created() {
+    this.getGoodsList()
+  },
+  methods: {
+    async getGoodsList() {
+      const { data: res } = await this.$http.get('goods', {
+        params: this.queryInfo
+      })
+      if (res.meta.status !== 200) {
+        return this.$message.error('获取商品列表失败')
+      }
+      this.$message.success('获取商品列表成功')
+      this.goodslist = res.data.goods
+      this.total = res.data.total
+    },
+    handleSizeChange(newSize) {
+      this.queryInfo.pagesize = newSize
+      this.getGoodsList()
+    },
+    handleCurrentChange(newPage) {
+      this.queryInfo.pagenum = newPage
+      this.getGoodsList()
+    },
+    async removeById(id){
+        const confirmResult = await this.$confirm(
+            '此操作将永久删除该商品,是否继续',
+            '提示',
+            {
+                confirmButtonText:'确定',
+                cancelButtonText:'取消',
+                type:'warning'
+            }
+        ).catch(err=>err)
+        if(confirmResult !== 'confirm'){
+            return this.$message.info('已经取消删除')
+        }
+        const {data:res} = await this.$http.delete('goods/${id}')
+        if(res.meta.status !== 200){
+            return this.$message.error('删除失败')
+        }
+        this.$message.success('删除成功')
+        this.getGoodsList()
+    },
+    goAddpage(){
+        this.$router.push('/goods/add')
+    }
+  }
+}
+</script>
+<style lang="less" scoped></style>
