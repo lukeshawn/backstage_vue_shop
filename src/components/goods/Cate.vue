5a460fd67262120393bcaab7e95831dfac0d4ebf
lukeshawn <lukeshawn1992@gmail.com>
2020-04-08 11:15:21 +0800

    完成cate页面

--- src/components/goods/Cate.vue
+++ src/components/goods/Cate.vue
@@ -1,5 +1,171 @@
 <template>
-    <div>
-        cate页面
-    </div>
+  <div>
+    <el-breadcrumb separator-class="el-icon-arrow-right">
+      <el-breadcrumb-item :to="{ path: '/home' }">首页</el-breadcrumb-item>
+      <el-breadcrumb-item>商品管理</el-breadcrumb-item>
+      <el-breadcrumb-item>商品分类</el-breadcrumb-item>
+    </el-breadcrumb>
+
+    <el-card>
+       <el-row>
+           <el-col>
+               <el-button type="primary" @click="showAddCateDialog">添加分类</el-button>
+           </el-col>
+       </el-row> 
+
+       <tree-table class="treeTable" :data="catelist" :columns="columns" :selection-type="false" :expand-type="false" show-index index-text="#" border :show-row-hover="false">
+           <template slot="isok" slot-scope="scope">
+               <i class="el-icon-success" v-if="scope.row.cat_deleted === false" style="color:lightgreen;" ></i>
+               <i class="el-icon-error" v-else style="color:red;"></i>
+           </template>
+           <template slot="order" slot-scope="scope">
+               <el-tag size="mini" v-if="scope.row.cat_level === 0">一级</el-tag>
+               <el-tag type="success" size="mini" v-else-if="scope.row.cat_level === 1">二级</el-tag>
+               <el-tag type="warning" size="mini" v-else>三级</el-tag>
+           </template>
+           <template slot="opt">
+               <el-button type="primary" icon="el-icon-edit" size="mini">编辑</el-button>
+               <el-button type="danger" icon="el-icon-delete" size="mini">删除</el-button>
+           </template>
+       </tree-table>
+
+       <el-pagination @size-change="handleSizeChange" @current-change="handleCurrentChange" :current-page="querInfo.pagenum" :page-sizes="[3,5,10,15]" :page-size="querInfo.pagesize" layout="total,sizes,prev,pager,next,jumper" :total="total">
+       </el-pagination>
+    </el-card>
+
+    <el-dialog title="添加分类" :visible.sync="addCateDialogVisible" width="50%" @close="addCateDialogClosed">
+        <el-form :model="addCateForm" :rules="addCateFormRules" ref="addCateFormRef" label-width="100px">
+            <el-form-item label="分类名称" prop="cat_name">
+                <el-input v-model="addCateForm.cat_name"></el-input>
+            </el-form-item>
+            <el-form-item label="父级分类">
+                <el-cascader expand-trigger="hover" :options="parentCateList" :props="cascaderProps" v-model="selectedKeys" @change="parentCateChanged" clearable change-on-select>
+                </el-cascader>
+            </el-form-item>
+        </el-form>
+        <span slot="footer" class="dialog-footer">
+            <el-button @click="addCateDialogVisible=false">取消</el-button>
+            <el-button type="primary" @click="addCate">确定</el-button>
+        </span>
+    </el-dialog>
+  </div>
 </template>
+
+<script>
+export default {
+    data(){
+        return {
+            querInfo:{
+                type:3,
+                pagenum:1,
+                pagesize:5
+            },
+            catelist:[],
+            total:0,
+            columns:[
+                {
+                    label:'分类名称',
+                    prop:'cat_name'
+                },
+                {
+                    label:'是否有效',
+                    type:'template',
+                    template:'isok'
+                },
+                {
+                    label:'操作',
+                    type:'template',
+                    template:'opt'
+                }
+            ],
+            addCateDialogVisible:false,
+            addCateForm: {
+                cat_name:'',
+                cat_pid:0,
+                cat_level:0
+            },
+            addCateFormRules: {
+                cat_name:[{required:true,message:'请输入分类名称',trigger:'blur'}]
+            },
+            parentCateList:[],
+            cascaderProps:{
+                value:'cat_id',
+                label:'cat_name',
+                children:'children'
+            },
+            selectedKeys:[]
+        }
+    },
+    created(){
+        this.getCateList()
+    },
+    methods:{
+        async getCateList(){
+            const {data:res} = await this.$http.get('categories',{
+                params:this.querInfo
+            })
+            if(res.meta.status !== 200){
+                return this.$message.error('获取商品分类失败')
+            }
+            this.catelist = res.data.result
+            this.total = res.data.total
+        },
+        handleSizeChange(newSize){
+            this.querInfo.pagesize = newSize
+            this.getCateList()
+        },
+        showAddCateDialog(){
+            this.getParentCateList()
+            this.addCateDialogVisible = true
+        },
+        async getParentCateList(){
+            const {data:res} = await this.$http.get('categories',{
+                params:{type:2}
+            })
+            if(res.meta.status !== 200){
+                return this.$message.error('获取父级分类数据失败')
+            }
+            this.parentCateList = res.data
+        },
+        parentCateChanged(){
+            if(this.selectedKeys.length > 0){
+                this.addCateForm.cat_pid = this.selectedKeys[this.selectedKeys.length-1]
+                this.addCateForm.cat_level = this.selectedKeys.length
+            }
+            else {
+                this.addCateForm.cat_pid = 0
+                this.addCateForm.cat_level = 0 
+            }
+        },
+        addCate(){
+            this.$refs.addCateFormRef.validate(async valid => {
+                if(!valid) return
+                const {data:res} = await this.$http.post('categories',this.addCateForm)
+                if(res.meta.status !== 201){
+                    return this.$message.error('添加分类失败')
+                }
+                this.$message.success('添加分类成功')
+                this.getCateList()
+                this.addCateDialogVisible = false
+            })
+        },
+        addCateDialogClosed(){
+            this.$refs.addCateFormRef.resetFields()
+            this.selectedKeys = []
+            this.addCateForm.cat_level = 0
+            this.addCateForm.cat_pid = 0
+        }
+
+    }
+}
+</script>
+
+<style lang="less" scoped>
+.treeTable {
+  margin-top: 15px;
+}
+
+.el-cascader {
+  width: 100%;
+}
+</style>
--- src/main.js
+++ src/main.js
@@ -8,6 +8,7 @@ import VueQuillEditor from 'vue-quill-editor'
 import 'quill/dist/quill.core.css'
 import 'quill/dist/quill.snow.css'
 import 'quill/dist/quill.bubble.css'
+import TreeTable from 'vue-table-with-tree-grid'
 
 import axios from 'axios'
 axios.defaults.baseURL = "http://127.0.0.1:8888/api/private/v1/"
@@ -36,6 +37,7 @@ Vue.filter('dateFormat', function(originVal) {
 })
 
 Vue.use(VueQuillEditor)
+Vue.component('tree-table', TreeTable)
 new Vue({
     router,
     render: h => h(App)
